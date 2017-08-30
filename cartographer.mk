723792c8a72bf2e498ec99ae660df7349159d525
Vikas Dhiman <dhiman@umich.edu>
2017-08-30 17:14:58 -0400

    install cartographer_ros.

--- cartographer.mk
+++ cartographer.mk
@@ -1,3 +1,4 @@
+
 CWD:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 PKG:=cartographer
 TPD:=$(HOME)/co/
@@ -5,9 +6,14 @@ VER:=0.2.1
 SD:=$(TPD)/$(PKG)-$(VER)
 IP?=$(TPD)/$(PKG)-$(VER)-install
 
-include ceres-solver.mk
 
-$(TPD)/$(PKG)/lib/libcartographer.a:
+RPKG:=cartographer_ros
+RTPD:=$(HOME)/co/
+RVER:=0.2.1
+RSD:=$(RTPD)/$(RPKG)-$(RVER)
+RIP?=$(RTPD)/$(RPKG)-$(RVER)-install
+
+all: $(RTPD)/$(RPKG)/bin/cartographer_node
 
 $(IP)/lib/libcartographer.a: $(SD)/build/Makefile
 	cd "$(<D)" && $(MAKE) install
@@ -15,9 +21,12 @@ $(IP)/lib/libcartographer.a: $(SD)/build/Makefile
 $(TPD)/$(PKG)/%: $(IP)/%
 	ln -fsT $(IP) $(TPD)/$(PKG)
 
+include $(CWD)/ceres-solver.mk
 $(SD)/build/Makefile: $(SD)/CMakeLists.txt $(Ceres_DIR)/CeresConfig.cmake
 	mkdir -p "$(@D)"
-	cd "$(@D)" && cmake .. -DCMAKE_INSTALL_PREFIX=$(IP) -DCeres_DIR=$(Ceres_DIR)
+	cd "$(@D)" \
+		&& module load gflags/2.2.1 glog/0.3.5 googletest/1.8.0 \
+		&& cmake .. -DCMAKE_INSTALL_PREFIX=$(IP) -DCeres_DIR=$(Ceres_DIR)
 
 $(TPD)/$(PKG)-$(VER)/CMakeLists.txt:
 	mkdir -p $(TPD)
@@ -27,3 +36,17 @@ $(TPD)/$(PKG)-$(VER)/CMakeLists.txt:
 
 echo-%:
 	@echo $($*)
+
+
+CMAKE_ROS:=$(RTPD)/$(RPKG)-$(RVER)/src/$(RPKG)-$(RVER)/CMakeLists.txt
+$(RSD)/devel/cartographer_ros/lib/cartographer_ros/cartographer_node: $(CMAKE_ROS) $(IP)/lib/libcartographer.a
+	cd "$(RSD)" \
+		&& sudo apt install ros-lunar-eigen-conversions ros-lunar-pcl-conversions ros-lunar-rosbag ros-lunar-tf2 ros-lunar-tf2-eigen ros-lunar-tf2-ros ros-lunar-urdf ros-lunar-visualization-msgs ros-lunar-rviz \
+		&& module load googletest \
+		&& source /opt/ros/lunar/setup.bash \
+		&& cartographer_DIR=$(IP)/share/cartographer/ catkin_make
+
+$(CMAKE_ROS):
+	mkdir -p $(RTPD)/$(RPKG)-$(RVER)/src/
+	git clone https://github.com/googlecartographer/cartographer_ros.git "$(@D)"
+	touch $@
--- ceres-solver.mk
+++ ceres-solver.mk
@@ -1,31 +1,31 @@
 SHELL:=/bin/bash -l
 CWD:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
-PKG:=ceres-solver
-TPD:=$(HOME)/co/
-VER:=1.9.0
-SD:=$(TPD)/$(PKG)-$(VER)
-IP?=$(TPD)/$(PKG)-$(VER)-install
-Ceres_DIR:=$(IP)/lib/cmake/Ceres
+Ceres_PKG:=ceres-solver
+Ceres_TPD:=$(HOME)/co/
+Ceres_VER:=1.9.0
+Ceres_SD:=$(Ceres_TPD)/$(Ceres_PKG)-$(Ceres_VER)
+Ceres_IP?=$(Ceres_TPD)/$(Ceres_PKG)-$(Ceres_VER)-install
+Ceres_DIR:=$(Ceres_IP)/lib/cmake/Ceres
 
-$(TPD)/$(PKG)/lib/cmake/Ceres/CeresConfig.cmake:
+$(Ceres_TPD)/$(Ceres_PKG)/lib/cmake/Ceres/CeresConfig.cmake:
 
-$(Ceres_DIR)/CeresConfig.cmake: $(SD)/build/Makefile
+$(Ceres_DIR)/CeresConfig.cmake: $(Ceres_SD)/build/Makefile
 	cd "$(<D)" && $(MAKE) install
 
-$(TPD)/$(PKG)/%: $(IP)/%
-	ln -fsT $(IP) $(TPD)/$(PKG)
+$(Ceres_TPD)/$(Ceres_PKG)/%: $(Ceres_IP)/%
+	ln -fsT $(Ceres_IP) $(Ceres_TPD)/$(Ceres_PKG)
 
 
-$(SD)/build/Makefile: $(SD)/CMakeLists.txt
+$(Ceres_SD)/build/Makefile: $(Ceres_SD)/CMakeLists.txt
 	mkdir -p "$(@D)"
 	cd "$(@D)" \
 		&& module load gflags/2.2.1 glog/0.3.5 \
-		&& cmake .. -DCMAKE_INSTALL_PREFIX=$(IP)
+		&& cmake .. -DCMAKE_INSTALL_PREFIX=$(Ceres_IP)
 
-$(TPD)/$(PKG)-$(VER)/CMakeLists.txt:
-	mkdir -p $(TPD)
+$(Ceres_TPD)/$(Ceres_PKG)-$(Ceres_VER)/CMakeLists.txt:
+	mkdir -p $(Ceres_TPD)
 	git clone https://ceres-solver.googlesource.com/ceres-solver "$(@D)"
-	cd "$(@D)" && git checkout $(VER)
+	cd "$(@D)" && git checkout $(Ceres_VER)
 	touch $@
 
 echo-%:
